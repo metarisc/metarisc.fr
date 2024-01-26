bb26cfc84bc9eb0b9223efe659be186079c56f25
Loup Mayeur <loupmayeur2003@gmail.com>
2024-01-26 16:57:42 +0100

    switching to SCSS

--- .eleventy.js
+++ .eleventy.js
@@ -1,19 +1,22 @@
 const pluginRss = require("@11ty/eleventy-plugin-rss");
+const sass = require("eleventy-sass");
 
 module.exports = function(eleventyConfig) {
   // Manage plugins
   eleventyConfig.addPlugin(pluginRss);
+  eleventyConfig.addPlugin(sass);
 
   // Copy the `img` and `css` folders to the output
   eleventyConfig.addPassthroughCopy("src/img");
-  eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/scripts");
-
+  
   // Copy all posts images
   eleventyConfig.addPassthroughCopy("src/posts/*/*.jpg");
-
+  
   // Copy error page
   eleventyConfig.addPassthroughCopy("src/error.html");
+  
+  // eleventyConfig.setBrowserSyncConfig({files: 'src/css/**/*.css'});
 
   return {
     templateFormats: ['html', 'liquid', 'md', 'njk'],
